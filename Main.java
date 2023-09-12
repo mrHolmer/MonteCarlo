87d12da256ac8a8993cbddf1b529771a0caa2ba0
Quincy Holmer <qholmer@schools.nyc.gov>
2023-09-12 09:07:44 -0400

    Updated through 1.2

--- Main.java
+++ Main.java
@@ -2,6 +2,25 @@ public class Main
   {
     public static void main(String[] args)
     {
-      System.out.println("hello");
+      System.out.println("Welcome to Monte Carlo!");
+      System.out.println("Brought to you by Enrico Fermi and Stanislaw Ulam");
+      System.out.println();
+      final double probabilityOfHeads = 0.5;
+      boolean isHeads = true;
+      int numberOfHeads = 0;
+      System.out.println("The probability of a fair coin landing on heads is " + probabilityOfHeads + ".");
+      System.out.println("The coin has landed heads " + numberOfHeads + " times.");
+      System.out.println();
+      System.out.println("I will now flip the coin.");
+      System.out.println("It's tails!");
+      isHeads = false;
+      System.out.println();
+      System.out.println("I will now flip the coin again.");
+      System.out.println("It's heads!");
+      isHeads = true;
+      numberOfHeads = numberOfHeads + 1;
+      System.out.println();
+      System.out.println("The coin has landed heads " + numberOfHeads + " times.");
+
     }
   }
