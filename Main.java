710d27b0f6355fc32d36fc1f9e39e036dfac3073
Quincy Holmer <qholmer@schools.nyc.gov>
2023-09-14 13:16:28 -0400

    Added compound assignment operator.

--- Main.java
+++ Main.java
@@ -3,19 +3,18 @@ public class Main
   {
     public static void main(String[] args)
     {
-//      System.out.println("Welcome to Monte Carlo!");
-//      System.out.println("Brought to you by Enrico Fermi and Stanislaw Ulam");
-//      System.out.println();
-//
-//      System.out.println("What is your name?");
-//      Scanner s = new Scanner(System.in);
-//      String name = s.nextLine();
-//      System.out.println("Hello, " + name + ". Heads or tails?");
-//      s = new Scanner(System.in);
-//      String call = s.nextLine();
-//      System.out.println(call + ", eh?");
-//      s.close();
-//      System.out.println();
+      System.out.println("Welcome to Monte Carlo!");
+      System.out.println("Brought to you by Enrico Fermi and Stanislaw Ulam");
+      System.out.println();
+
+      System.out.println("What is your name?");
+      Scanner s = new Scanner(System.in);
+      String name = s.nextLine();
+      System.out.println("Hello, " + name + ". Heads or tails?");
+      s = new Scanner(System.in);
+      String call = s.nextLine();
+      System.out.println(call + ", eh?");
+      System.out.println();
 
       final double probabilityOfHeads = 0.5;
       boolean isHeads = true;
@@ -25,20 +24,23 @@ public class Main
       System.out.println("The coin has landed heads " + numberOfHeads + " times.");
       System.out.println();
 
-      System.out.println("I will now flip the coin.");
-      System.out.println("It's tails!");
-      numberOfTails += 1; //compound assignment operator
+      System.out.println("How many times would you like to flip the coin?");
+      int flips = s.nextInt();
+      System.out.println("It was tails every time!");
+      numberOfTails += flips; //compound assignment operator
       isHeads = false;
       System.out.println();
 
       System.out.println("I will now flip the coin again.");
-      System.out.println("It's heads!");
+      System.out.println("It was heads!");
       isHeads = true;
       numberOfHeads++; //increment operator
       System.out.println();
 
       System.out.println("The coin has landed heads " + numberOfHeads + " time.");
-      double proportionOfTails = numberOfTails/(numberOfTails + numberOfHeads + 0.0); //compound expression
+      double proportionOfTails = (numberOfTails)/(numberOfTails + numberOfHeads + 0.0); //compound expression
       System.out.println("The coin has landed tails " + proportionOfTails * 100 + " percent of the time.");
+
+
     }
   }
