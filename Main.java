4dea6afa9dd25a5ef9684886c3eb61c27579d3a6
Quincy Holmer <qholmer@schools.nyc.gov>
2023-09-18 09:31:14 -0400

    Added Coin.java class

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main
   {
     public static void main(String[] args)
     {
+      Coin coin = new Coin();
+
       System.out.println("Welcome to Monte Carlo!");
       System.out.println("Brought to you by Enrico Fermi and Stanislaw Ulam");
       System.out.println();
@@ -16,31 +18,14 @@ public class Main
       System.out.println(call + ", eh?");
       System.out.println();
 
-      final double probabilityOfHeads = 0.5;
-      boolean isHeads = true;
-      int numberOfHeads = 0;
-      int numberOfTails = 0;
-      System.out.println("The probability of a fair coin landing on heads is " + probabilityOfHeads + ".");
-      System.out.println("The coin has landed heads " + numberOfHeads + " times.");
+      coin.flip();
       System.out.println();
 
       System.out.println("How many times would you like to flip the coin?");
       int flips = s.nextInt();
-      System.out.println("It was tails every time!");
-      numberOfTails += flips; //compound assignment operator
-      isHeads = false;
+      coin.tails(flips);
       System.out.println();
 
-      System.out.println("I will now flip the coin again.");
-      System.out.println("It was heads!");
-      isHeads = true;
-      numberOfHeads++; //increment operator
-      System.out.println();
-
-      System.out.println("The coin has landed heads " + numberOfHeads + " time.");
-      double proportionOfTails = (numberOfTails)/(numberOfTails + numberOfHeads + 0.0); //compound expression
-      System.out.println("The coin has landed tails " + proportionOfTails * 100 + " percent of the time.");
-
-
+      coin.printTailsProportion();
     }
   }
